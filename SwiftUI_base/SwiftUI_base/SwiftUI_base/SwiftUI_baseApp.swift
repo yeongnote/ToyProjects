2fae180fd0bcf1a1fbf9e6041359110b54445bf8
yeongho ha <132809141+yeongnote@users.noreply.github.com>
2024-04-29 16:57:12 +0900

    git init

---
+++ SwiftUI_base/SwiftUI_base/SwiftUI_base/SwiftUI_baseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_baseApp.swift
+//  SwiftUI_base
+//
+//  Created by YeongHo Ha on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_baseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
